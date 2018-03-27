a2f9fedcfe309a0d6ef0abbd6547cb6e5c4ef177
mcknight816 <mcknight816@gmail.com>
2018-03-27 08:40:33 -0400

    Introduce Example sites

--- app/components/modules/ecommerce/cart.js
+++ app/components/modules/ecommerce/cart.js
@@ -1,36 +1,136 @@
-//Main Controller
-catwalkApp.controller('shop-controller', ['$scope','USettings',
-    function ($scope,USettings) {
-        angular.element('.nav-side-menu').css('background', '#37474F');
-        angular.element('.page-topbar').css('background', '#37474F');
-        angular.element('.nav-side-menu').css('margin-top', '60px');
-        USettings.get().then(function(data){
-            $scope.settings = data;
-            $scope.base_url = base_url;
-        });
+
+catwalkApp.controller('cart-controller', ['$scope','$location','$stateParams','conduit',
+    function ($scope,$location,$stateParams,conduit) {
+        $scope.srchterm = '';
+        $scope.collection = conduit.collection('cart','');
+        $scope.listParams = {
+            or:false,
+            filterByFields: "{}",
+            page: 1,
+            sord: "ASC",
+            projection: {},
+            rows: 20,
+            sidx: "title"
+        };
+        $scope.totalpages = 0;
+
+        $scope.$watch('srchterm', function(newVal, oldVal) {
+            $scope.search();
+        }, true);
+
+        $scope.sort = function(){
+            if($scope.listParams.sord ==='ASC'){
+                $scope.listParams.sord = 'DESC';
+            }else{
+                $scope.listParams.sord = 'ASC';
+            }
+            $scope.search();
+        };
+
+        $scope.search = function(){
+            var filterByFields = {'$or':[]};
+            var search = $scope.srchterm;
+            //lets search on subcategory, category && title
+            $scope.listParams.page = 1;
+            if( search&& search !== '' ){
+                filterByFields['$or'].push({'subcategory':{'$regex':search,'$options':'i'}});
+                filterByFields['$or'].push({'category':{'$regex':search,'$options':'i'}});
+                filterByFields['$or'].push({'title':{'$regex':search,'$options':'i'}});
+                $scope.listParams['projection']['subcategory'] = 1;
+                $scope.listParams['projection']['category'] = 1;
+                $scope.listParams['projection']['title'] = 1;
+                $scope.listParams['filterByFields'] =  filterByFields;
+            }else{
+                $scope.listParams['projection'] = {};
+                $scope.listParams['filterByFields'] = {};
+            }
+            $scope.list();
+        };
+        $scope.cycle= function(){
+            if($scope.interval){
+                clearInterval($scope.interval);
+                $scope.interval = null;
+            }else{
+                $scope.interval = setInterval(function() {
+                    $scope.nextPage();
+                }, 1500);
+            }
+        };
+        $scope.nextPage = function(){
+            $scope.setPage($scope.listParams.page + 1);
+        };
+        $scope.prevPage = function(){
+            $scope.setPage($scope.listParams.page - 1);
+        };
+
+        $scope.setPage = function(page){
+            $scope.listParams.page = page;
+            $scope.list();
+        };
+
+        $scope.list = function(){
+            $scope.collection.get($scope.listParams ).then(function(data){
+                $scope.items = data.rows;
+                $scope.listParams.page = data.currpage;
+                $scope.totalpages = data.totalpages;
+            });
+        };
+
+        $scope.get = function(id){
+            $scope.collection.getById(id).then(function(cart) {
+                $scope.modelData = cart;
+                $scope.imageSrc = "";
+            });
+        };
+
+        $scope.save = function(){
+            console.log($scope.modelData);
+            $scope.collection.save($scope.modelData).then(function(){
+                $scope.back();
+            });
+        };
+
+        $scope.remove = function(id){
+            $scope.collection.remove(id).then(function () {
+                $scope.list();
+            });
+        };
+
+        $scope.back = function () {
+            window.history.back();
+        };
+
+        $scope.new= function(){
+            $location.path('/ecom/cart/');
+        };
+
+        $scope.update= function(id){
+            $location.path('/ecom/cart/' + id);
+        };
+
+        if($stateParams.id){ $scope.get($stateParams.id);}
+        else{ $scope.list();}
+
     }
 ]);
-/**
- *   Routing
- */
+
 catwalkApp.config(['$stateProvider', '$urlRouterProvider','USER_ROLES',
     function ($stateProvider, $urlRouterProvider,USER_ROLES) {
         $stateProvider
-            .state('shop', {
-                url: "/shop",
-                views: {
-                    'header': {
-                        templateUrl:'components/modules/ecommerce/templates/header.html',
-                        controller:'ecommerce-controller as main'
-                    },
-                    'side': {
-                        templateUrl:'components/modules/ecommerce/templates/side-bar.html',
-                        controller:'ecommerce-controller as main'
-                    },
-                    'content': {
-                        template:'<div class="has-sidebar has-topbar" ui-view></div>'
-                    }
-                }
+            .state('ecom.carts', {
+                url: "/carts",
+                templateUrl: "components/modules/ecommerce/templates/cart-list.html",
+                controller: 'cart-controller'
+            })
+            .state('ecom.new_cart', {
+                url: "/cart",
+                templateUrl: "components/modules/ecommerce/templates/cart.html",
+                controller: 'cart-controller'
+            })
+            .state('ecom.cart', {
+                url: "/cart/:id",
+                templateUrl: "components/modules/ecommerce/templates/cart.html",
+                controller: 'cart-controller'
             })
     }
-]).run(securityHandler);
+]);
